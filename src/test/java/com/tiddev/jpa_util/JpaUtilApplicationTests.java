768e78995d94e9e5d75930fc989aadc7f0090414
dahmardeh.a <dahmardeh.a@tiddev.com>
2025-07-16 13:14:41 +0330

    feat(JpaUtilApplicationTests): JpaUtilApplicationTests created

---
+++ src/test/java/com/tiddev/jpa_util/JpaUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiddev.jpa_util;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaUtilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
